75c87b5079e124e00b96aa38209b5e8f26054e81
TrianguloY <correo--correo@hotmail.com>
2023-12-11 00:04:44 +0100

    replace messages with data

--- mobile/src/main/java/de/rhaeus/dndsync/DNDNotificationService.java
+++ mobile/src/main/java/de/rhaeus/dndsync/DNDNotificationService.java
@@ -5,23 +5,14 @@ import android.content.SharedPreferences;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
-import androidx.annotation.NonNull;
-import androidx.preference.PreferenceManager;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.android.gms.wearable.CapabilityClient;
-import com.google.android.gms.wearable.CapabilityInfo;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.Wearable;
 
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
+import androidx.preference.PreferenceManager;
+
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 public class DNDNotificationService extends NotificationListenerService {
     private static final String TAG = "DNDNotificationService";
-    private static final String DND_SYNC_CAPABILITY_NAME = "dnd_sync";
     private static final String DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync";
 
     public static boolean running = false;
@@ -82,53 +73,12 @@ public class DNDNotificationService extends NotificationListenerService {
     }
 
     private void sendDNDSync(int dndState) {
-        // https://developer.android.com/training/wearables/data/messages
-
-        // search nodes for sync
-        CapabilityInfo capabilityInfo = null;
-        try {
-            capabilityInfo = Tasks.await(
-                    Wearable.getCapabilityClient(this).getCapability(
-                            DND_SYNC_CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-            Log.e(TAG, "execution error while searching nodes", e);
-            return;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Log.e(TAG, "interruption error while searching nodes", e);
-            return;
-        }
-
-        // send request to all reachable nodes
-        // capabilityInfo has the reachable nodes with the dnd sync capability
-        Set<Node> connectedNodes = capabilityInfo.getNodes();
-        if (connectedNodes.isEmpty()) {
-            // Unable to retrieve node with transcription capability
-            Log.d(TAG, "Unable to retrieve node with sync capability!");
-        } else {
-            for (Node node : connectedNodes) {
-                if (node.isNearby()) {
-                    byte[] data = new byte[1];
-                    data[0] = (byte) dndState;
-                    Task<Integer> sendTask =
-                            Wearable.getMessageClient(this).sendMessage(node.getId(), DND_SYNC_MESSAGE_PATH, data);
-
-                    sendTask.addOnSuccessListener(new OnSuccessListener<Integer>() {
-                        @Override
-                        public void onSuccess(Integer integer) {
-                            Log.d(TAG, "send successful! Receiver node id: " + node.getId());
-                        }
-                    });
-
-                    sendTask.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "send failed! Receiver node id: " + node.getId());
-                        }
-                    });
-                }
-            }
-        }
+        // https://developer.android.com/training/wearables/data/data-items
+        Wearable.getDataClient(this)
+                .putDataItem(PutDataRequest.create(DND_SYNC_MESSAGE_PATH)
+                        .setData(new byte[]{(byte) dndState, 0})
+                        // mark urgent, otherwise it could take up to 30 minutes to sync
+                        .setUrgent()
+                );
     }
 }
--- mobile/src/main/java/de/rhaeus/dndsync/DNDSyncListenerService.java
+++ mobile/src/main/java/de/rhaeus/dndsync/DNDSyncListenerService.java
@@ -2,31 +2,27 @@ package de.rhaeus.dndsync;
 
 import android.app.NotificationManager;
 import android.content.Context;
-import android.os.Handler;
-import android.os.PowerManager;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.WearableListenerService;
 
 public class DNDSyncListenerService extends WearableListenerService {
     private static final String TAG = "DNDSyncListenerService";
-    private static final String DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync";
-
 
     @Override
-    public void onMessageReceived (@NonNull MessageEvent messageEvent) {
-        Log.d(TAG, "onMessageReceived: " + messageEvent);
+    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
+        Log.d(TAG, "onDataChanged: " + dataEventBuffer);
 
-        if (messageEvent.getPath().equalsIgnoreCase(DND_SYNC_MESSAGE_PATH)) {
-            Log.d(TAG, "received path: " + DND_SYNC_MESSAGE_PATH);
+        for (DataEvent dataEvent : dataEventBuffer) {
 
-            byte[] data = messageEvent.getData();
+            // No need to filter by path, it is defined in the manifest
+            // Android will make sure we only get our own messages
+
+            byte[] data = dataEvent.getDataItem().getData();
             // data[0] contains dnd mode of phone
             // 0 = INTERRUPTION_FILTER_UNKNOWN
             // 1 = INTERRUPTION_FILTER_ALL (all notifications pass)
@@ -35,6 +31,11 @@ public class DNDSyncListenerService extends WearableListenerService {
             // 4 = INTERRUPTION_FILTER_ALARMS
             byte dndStatePhone = data[0];
             Log.d(TAG, "dndStatePhone: " + dndStatePhone);
+
+            if(dndStatePhone == 5 || dndStatePhone == 6){
+                // our own dnd message, just ignore
+                continue;
+            }
 
             // get dnd state
             NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -52,12 +53,10 @@ public class DNDSyncListenerService extends WearableListenerService {
                     mNotificationManager.setInterruptionFilter(dndStatePhone);
                     Log.d(TAG, "DND set to " + dndStatePhone);
                 } else {
-                Log.d(TAG, "attempting to set DND but access not granted");
+                    Log.d(TAG, "attempting to set DND but access not granted");
                 }
             }
 
-        } else {
-            super.onMessageReceived(messageEvent);
         }
     }
 
